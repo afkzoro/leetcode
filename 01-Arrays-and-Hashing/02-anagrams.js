cc21d53d6eeafda298fb6a12dc0a48ab5e817ce1
afkzoro <samueltheophilus019@gmail.com>
2025-01-01 22:10:21 +0100

    anagrams finder 0(n) + O(n^2) solution

--- 01-Arrays-and-Hashing/02-anagrams.js
+++ 01-Arrays-and-Hashing/02-anagrams.js
@@ -4,5 +4,38 @@ class Solution {
      * @param {string} t
      * @return {boolean}
      */
-    isAnagram(s, t) {}
+    isAnagram(s, t) {
+      if (s.length !== t.length ) {
+        return false
+      }
+      
+      // O(n^2) solution
+      // let tArray = t.split('')
+      // for (var char of s){
+      //   var charIndexintArray = tArray.indexOf(char)
+      //   if (charIndexintArray === -1 ){
+      //     return false
+      //   }
+      //   tArray = t.replace(char, '')
+      // }
+      
+      // return true
+      
+      // O(n) solution
+      
+      const charCount = {}
+      
+      for (var char of s)(
+        charCount[char] = (charCount[char] || 0) + 1
+      )
+      
+      for (var char of t) {
+        if (!charCount[char]) {
+          return false
+        }
+        charCount[char]--
+      }
+      
+      return true
+    }
 }
