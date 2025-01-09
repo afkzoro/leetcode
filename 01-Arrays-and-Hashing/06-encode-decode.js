cd650773f283e399d7cce7e95a02568334d4ee50
afkzoro <samueltheophilus019@gmail.com>
2025-01-09 21:08:33 +0100

    solve encode and decode

---
+++ 01-Arrays-and-Hashing/06-encode-decode.js
@@ -0,0 +1,29 @@
+class Solution {
+    /**
+     * @param {string[]} strs
+     * @returns {string}
+     */
+    encode(strs) {
+        const lengths = strs.map(str => str.length + ',').join('')
+        return lengths + '#' + strs.join('')
+    }
+    
+    /**
+     * @param {string} str
+     * @returns {string[]}
+     */
+    decode(str) {
+        const decoded = []
+        const [lengths, concatenated] = str.split('#')
+        const lengthsArray = lengths.split(',').filter(x => x)
+        
+        let start = 0
+        for (let length of lengthsArray) {
+            length = parseInt(length)
+            decoded.push(concatenated.slice(start, start + length))
+            start += length
+        }
+        
+        return decoded
+    }
+}
